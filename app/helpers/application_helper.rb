bd7fb892ef1a445a6dee1285e5f45126317c58f0
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-23 03:17:05 +0300

    add application methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def user_avatar(user)
+    asset_path('user.png')
+  end
+
+  def bootstrap_flash(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade show", role: 'alert') do
+        concat content_tag(:button, '', class: 'btn-close', data: { 'bs-dismiss': 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
+  private
+
+  def bootstrap_class_for(flash_type)
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-success'
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
 end

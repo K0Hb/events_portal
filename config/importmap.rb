fd84714a3806d432064f6a8f4d76ccca4cd767a7
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-08-31 20:34:29 +0300

    add yandex map

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,8 @@
 
 pin "application", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js" # use unpkg.com as ga.jspm.io contains a broken popper package
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
+pin "scripts/maps";
+
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from 'api-maps', to: "https://api-maps.yandex.ru/2.1/?lang=ru_RU"

6da0f47cbc736935c2c945d6655369ba8f4635da
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-09-19 21:15:26 +0300

    edit policy test

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe EventPolicy, type: :policy do
-  let(:current_user1) { User.new(id: 1) }
-  let(:current_user2) { User.new(id: 2) }
-  let(:event) { Event.new(user: current_user1) }
+  let(:current_user1) { FactoryBot.create(:user) }
+  let(:current_user2) { FactoryBot.create(:user) }
+
+  let(:event) { FactoryBot.create(:event, user: current_user1) }
+  let(:event_with_pin) { FactoryBot.create(:event, user: current_user1, pincode: '1234') }
+
+  let(:cookie) { { "events_#{event_with_pin.id}_pincode" => '1234' } }
+
+  let(:owner) { UserContext.new(current_user1, {}) }
+  let(:not_owner) { UserContext.new(current_user2, {}) }
+  let(:not_owner_with_pin) { UserContext.new(current_user2, cookie) }
+  let(:anonimous_user) { UserContext.new(nil, {}) }
 
   subject { described_class }
 
@@ -11,7 +20,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'when user is not owner' do
       permissions :edit?, :update?, :destroy? do
         it 'not permit' do
-          is_expected.not_to permit(current_user2, event)
+          is_expected.not_to permit(not_owner, event)
         end
       end
     end
@@ -19,7 +28,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'when user is not logged' do
       permissions :edit?, :update?, :destroy? do
         it 'not permit' do
-          is_expected.not_to permit(nil, Event)
+          is_expected.not_to permit(anonimous_user, event)
         end
       end
     end
@@ -27,7 +36,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'when user is the owner' do
       permissions :edit?, :destroy?, :update? do
         it 'permit' do
-          is_expected.to permit(current_user1, event)
+          is_expected.to permit(owner, event)
         end
       end
     end
@@ -37,10 +46,10 @@ RSpec.describe EventPolicy, type: :policy do
     context 'registered user create event' do
       permissions :create? do
         it 'permit' do
-          is_expected.to permit(current_user1, :event)
+          is_expected.to permit(owner, Event)
         end
         it 'permit' do
-          is_expected.to permit(current_user2, :event)
+          is_expected.to permit(not_owner, Event)
         end
       end
     end
@@ -48,7 +57,7 @@ RSpec.describe EventPolicy, type: :policy do
     context 'not registered user create event' do
       permissions :create? do
         it 'not permit' do
-          is_expected.not_to permit(nil, Event)
+          is_expected.not_to permit(anonimous_user, Event)
         end
       end
     end

423f4b9a85a4ae1e4b41c601ca0c9e24a3bd1cfb
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-09-20 11:09:13 +0300

    add self.find_for_github_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable,
-    :omniauthable, omniauth_providers: [:facebook, :vkontakte, :github]
+    :omniauthable, omniauth_providers: %i[facebook vkontakte github]
 
   has_many :events, dependent: :destroy
   has_many :comments, dependent: :destroy
@@ -46,6 +46,18 @@ class User < ApplicationRecord
       user.email = email
       user.password = Devise.friendly_token.first(16)
     end
+  end
+
+  def self.find_for_github_oauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user
   end
 
   private

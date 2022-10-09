28bf6c84a2248dca0ff818a286015a2906e9e3e5
Vjacheslav <goplit2010.konovalov@yandex.ru>
2022-10-09 08:50:09 +0300

    refactoring classs methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,39 +16,44 @@ class User < ApplicationRecord
 
   after_commit :link_subscriptions, on: :create
 
-  def self.find_or_create_for_oauth(access_token)
-    provider = access_token.provider
-    name = access_token.info.name
-    email = access_token.info.email
-    url = get_url_from_access_token(access_token)
-    image = URI.parse(access_token.info.image).open if access_token.info.image
-
-    user = find_by(email: email)
-
-    return user if user.present?
-    where(url: url, provider: provider).first_or_create! do |user|
-      user.name = name
-      user.email = email
-      user.password = Devise.friendly_token.first(16)
-      user.url = url
-      user.avatar.attach(io: image, filename: 'avatar.png') if image
-    end
-  end
-
   private
-
-  def self.get_url_from_access_token(access_token)
-    case access_token.provider.to_sym
-    when :yandex
-      "https://yandex/#{access_token.info.name}"
-    when :github
-      access_token.info.urls['GitHub']
-    when :vkontakte
-      access_token.info.urls['Vkontakte']
-    end
-  end
 
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
+  end
+
+  class << self
+    def find_or_create_for_oauth(access_token)
+      provider = access_token.provider
+      name = access_token.info.name
+      email = access_token.info.email
+      url = get_url_from_access_token(access_token)
+      image = URI.parse(access_token.info.image).open if access_token.info.image
+
+      user = find_by(email: email)
+
+      return user if user.present?
+
+      where(url: url, provider: provider).first_or_create! do |user|
+        user.name = name
+        user.email = email
+        user.password = Devise.friendly_token.first(16)
+        user.url = url
+        user.avatar.attach(io: image, filename: 'avatar.png') if image
+      end
+    end
+
+    private
+
+    def get_url_from_access_token(access_token)
+      case access_token.provider.to_sym
+      when :yandex
+        "https://yandex/#{access_token.info.name}"
+      when :github
+        access_token.info.urls['GitHub']
+      when :vkontakte
+        access_token.info.urls['Vkontakte']
+      end
+    end
   end
 end
